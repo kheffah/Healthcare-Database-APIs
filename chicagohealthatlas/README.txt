2d52887a3c0c715e6e94352d17151d526d6e6dee
Mohamed Amgad Tageldin <mtageld@emory.edu>
2019-08-18 23:13:43 -0400

    chicagohealthatlas interaction utils

---
+++ chicagohealthatlas/README.txt
@@ -0,0 +1,16 @@
+STEP 1:
+open terminal
+
+STEP 2: 
+Type the following:
+
+conda activate chicago
+cd ~/Desktop/chicagohealthatlas/
+
+STEP 3:
+Edit config.py file as you please
+
+STEP 4:
+type the following in the terminal
+
+python utils.py
---
+++ chicagohealthatlas/configs.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+SAVEPATH = "./results/"
+#SAVEPATH = "/content/drive/My Drive/Colab Notebooks/results/"
+
+GET_INDICATORS = True
+GET_PLACES = True
+GET_TOPICS = True
+GET_TOPIC_SUBCATEGORIES = True
+GET_INDICATORS = True
+GET_SUMMARY_TABLES = True
+
+# What indicators to fetch
+INDICATOR_LIST = None
+#INDICATOR_LIST = [
+#  'use-of-alternative-medicine',
+## 'reduced-medication-dosage-due-to-cost',
+## 'colorectal-cancer-incidence',
+## 'total-population', 
+#  'non-hispanic-african-american-or-black',
+#]
+
+# What geographies to fetch
+GEO_LIST = None
+#GEO_LIST = [
+#  'norwood-park',
+#  'albany-park',
+#  'ashburn'
+#]
